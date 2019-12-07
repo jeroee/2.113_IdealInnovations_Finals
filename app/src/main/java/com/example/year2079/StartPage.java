bb5e3588b29fc68576346b6c00743402142c43a0
jeroee <jeroejeroe@gmail.com>
2019-12-08 01:11:58 +0800

    added StartPage with options branching to checklist and group member info

---
+++ app/src/main/java/com/example/year2079/StartPage.java
@@ -0,0 +1,49 @@
+package com.example.year2079;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartPage extends AppCompatActivity {
+
+    Button start;
+    Button checklist;
+    Button us;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_page);
+
+        start=findViewById(R.id.start);
+        checklist=findViewById(R.id.checklist);
+        us=findViewById(R.id.us);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent go = new Intent(StartPage.this, Year2079.class);
+                startActivity(go);
+            }
+        });
+
+        checklist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent list = new Intent(StartPage.this, Scope.class);
+                startActivity(list);
+            }
+        });
+
+        us.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent us = new Intent(StartPage.this, GroupMembers.class);
+                startActivity(us);
+            }
+        });
+    }
+}
