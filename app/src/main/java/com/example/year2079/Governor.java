4f1b8412619019385b4fb6e6dff46b4fd7a53bbe
jeroee <jeroejeroe@gmail.com>
2019-12-09 17:40:34 +0800

    resize fonts

--- app/src/main/java/com/example/year2079/Governor.java
+++ app/src/main/java/com/example/year2079/Governor.java
@@ -3,12 +3,18 @@ package com.example.year2079;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class Governor extends AppCompatActivity {
-
+    TextView content;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_governor);
+
+        content = findViewById(R.id.govcontent);
+
+        content.setMovementMethod(new ScrollingMovementMethod());
     }
 }
