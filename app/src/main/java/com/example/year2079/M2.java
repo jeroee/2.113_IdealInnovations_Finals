b5039cabb0245a2e6d87caa1ef8379a6b6ed2785
apronoob88 <apronoob88@gmail.com>
2019-12-10 22:13:51 +0800

    changed 2 text

--- app/src/main/java/com/example/year2079/M2.java
+++ app/src/main/java/com/example/year2079/M2.java
@@ -4,14 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class M2 extends AppCompatActivity {
 
     Button m2_1;
     Button m2_2;
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +21,10 @@ public class M2 extends AppCompatActivity {
 
         m2_1=findViewById(R.id.m2_1);
         m2_2=findViewById(R.id.m2_2);
+        textView=findViewById(R.id.textView8);
+        textView.setMovementMethod(new ScrollingMovementMethod());
+
+
 
         m2_1.setOnClickListener(new View.OnClickListener() {
             @Override
