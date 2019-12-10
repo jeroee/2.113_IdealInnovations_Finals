c90e68b9d0808f4989a40b6b719a443c91eb09f6
jeroee <jeroejeroe@gmail.com>
2019-12-10 17:00:02 +0800

    l

--- app/src/main/java/com/example/year2079/G2_1.java
+++ app/src/main/java/com/example/year2079/G2_1.java
@@ -3,12 +3,19 @@ package com.example.year2079;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class G2_1 extends AppCompatActivity {
+
+    TextView abc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_g2_1);
+
+        abc=findViewById(R.id.textView5);
+        abc.setMovementMethod(new ScrollingMovementMethod());
     }
 }
--- app/src/main/java/com/example/year2079/M1_1_2.java
+++ app/src/main/java/com/example/year2079/M1_1_2.java
@@ -3,12 +3,22 @@ package com.example.year2079;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class M1_1_2 extends AppCompatActivity {
+
+    TextView m1_1_2content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_m1_1_2);
+
+        m1_1_2content=findViewById(R.id.m1_1_2content);
+
+        m1_1_2content.setMovementMethod(new ScrollingMovementMethod());
     }
 }
